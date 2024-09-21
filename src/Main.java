2da69d3610327bb4f09257f470aba364932c53a9
Censtro <eidenbecker@gmail.com>
2024-09-21 23:01:21 +0300

    Добавлена реализация паттерна Декоратор для чтения файлов целиком.
    Добавлен интерфейс FileReader для описания чтения файлов.
    
    Создан класс TextFileReader, реализующий чтение
    файла целиком в память.
    
    Добавлены декораторы:
    Реализован абстрактный декоратор FileReaderDecorator
    для расширения функциональности чтения.
    EncryptedFileReader для расшифровки содержимого файла (Base64).

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import FilesIO_and_decorators.*;
+
+import java.io.IOException;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        FileReader PlainTextReader = new PlainTextReader("input.txt");
+        try
+        {
+            System.out.println(PlainTextReader.read());
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
